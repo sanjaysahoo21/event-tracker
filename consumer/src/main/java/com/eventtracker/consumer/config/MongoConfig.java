acb52291d78623af50e3998c0e1eddcec401a209
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2026-03-07 10:44:13 +0530

    initialized mongoConfig.java

---
+++ consumer/src/main/java/com/eventtracker/consumer/config/MongoConfig.java
@@ -0,0 +1,41 @@
+package com.eventtracker.consumer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.Date;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(Arrays.asList(
+                new OffsetDateTimeWriteConverter(),
+                new OffsetDateTimeReadConverter()
+        ));
+    }
+
+    @WritingConverter
+    static class OffsetDateTimeWriteConverter implements Converter<OffsetDateTime, Date> {
+        @Override
+        public Date convert(OffsetDateTime source) {
+            return Date.from(source.toInstant());
+        }
+    }
+
+    @ReadingConverter
+    static class OffsetDateTimeReadConverter implements Converter<Date, OffsetDateTime> {
+        @Override
+        public OffsetDateTime convert(Date source) {
+            return source.toInstant().atOffset(ZoneOffset.UTC);
+        }
+    }
+}
